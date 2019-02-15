a88b4176ba0fa548633cbea2f89254f3739c0a91
David Amigo <david.amigo@privalia.com>
2019-02-15 14:25:19 +0100

    Created Spring Boot project structure

---
+++ src/main/java/com/privalia/poc/avrodemo/AvrodemoApplication.java
@@ -0,0 +1,12 @@
+package com.privalia.poc.avrodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvrodemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvrodemoApplication.class, args);
+    }
+}
