73425c451fe8cea9b207ee1b8a807d077f77d306
David Amigo <david.amigo@privalia.com>
2019-02-19 15:26:11 +0100

    Dufault controller splitted to create a new controller CustomersV1 with actions to list/create/view customers

---
+++ src/main/java/com/privalia/poc/avrodemo/controller/CustomerV1Controller.java
@@ -0,0 +1,105 @@
+package com.privalia.poc.avrodemo.controller;
+
+import com.privalia.poc.avrodemo.avro.Customer;
+import com.privalia.poc.avrodemo.kafka.producer.CustomerKafkaAvroProducer;
+import com.privalia.poc.avrodemo.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Collection;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Controller for Customer V1 - Route: /customers
+ *
+ * @author david.amigo
+ */
+@Controller
+@RequestMapping("/customers/v1")
+public class CustomerV1Controller {
+
+    /** The Kafka AVRO customers producer */
+    private CustomerKafkaAvroProducer customerKafkaAvroProducer;
+
+    /** The customers repository */
+    private CustomerRepository customerRepository;
+
+    /**
+     * Default constructor
+     *
+     * @param customerKafkaAvroProducer the Kafka AVRO customers producer
+     * @param customerRepository        the customers repository
+     */
+    @Autowired
+    public CustomerV1Controller(
+            CustomerKafkaAvroProducer customerKafkaAvroProducer,
+            CustomerRepository customerRepository
+    ) {
+        this.customerKafkaAvroProducer = customerKafkaAvroProducer;
+        this.customerRepository = customerRepository;
+    }
+
+    /**
+     * Action GET / - Default action - shows the homepage
+     *
+     * @param model the model to send to the view
+     * @return the name of the template to load (list.html)
+     */
+    @GetMapping({"", "/"})
+    public String loistCustomersAction(Model model) {
+        Collection<Customer> customers = customerRepository.findAll();
+        model.addAttribute("customers", customers);
+        return "customers-v1/list";
+    }
+
+    /**
+     * Action GET /{id} - Shows the customer data
+     *
+     * @param model the model to send to the view
+     * @param id    the id of the customer
+     * @return the name of the template to load (list.html)
+     */
+    @GetMapping("/{id}")
+    public String showCustomerAction(Model model, @PathVariable("id") String id) {
+        Customer customer = customerRepository.find(id);
+        model.addAttribute("customer", customer);
+        return "customers-v1/view";
+    }
+
+    /**
+     * Action GET /create - Shows the form for creating a cutomer
+     *
+     * @param model the model to send to the view
+     * @return the name of the template to load (create.html)
+     */
+    @GetMapping("/create")
+    public String createCustomerAction(Model model) {
+        model.addAttribute("customer", new Customer());
+        return "customers-v1/create";
+    }
+
+    /**
+     * Action POST /create - Creates the customer - sends the customer to the Kafka topic
+     *
+     * @param customer the customer data
+     * @return Redirecto to another page
+     */
+    @PostMapping("/create")
+    public ModelAndView postCustomerAction(
+            @ModelAttribute Customer customer,
+            @Value("${sprint.kafka.topic.customers.topic-name}") String topic
+    ) {
+        try {
+            customer.setUuid(UUID.randomUUID().toString());
+            customerKafkaAvroProducer.send(topic, customer);
+        } catch (ExecutionException | InterruptedException exc) {
+            exc.printStackTrace();
+        }
+        return new ModelAndView("redirect:/customers/v1");
+    }
+}
--- src/main/java/com/privalia/poc/avrodemo/controller/DefaultController.java
+++ src/main/java/com/privalia/poc/avrodemo/controller/DefaultController.java
@@ -2,74 +2,35 @@ package com.privalia.poc.avrodemo.controller;
 
 import com.privalia.poc.avrodemo.avro.Customer;
 import com.privalia.poc.avrodemo.kafka.producer.CustomerKafkaAvroProducer;
+import com.privalia.poc.avrodemo.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Collection;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
+/**
+ * Default controller - Route: /
+ *
+ * @author david.amigo
+ */
 @Controller
 @RequestMapping("/")
 public class DefaultController {
 
-    /** The Kafka producer */
-    private CustomerKafkaAvroProducer kafkaAvroProducer;
-
-    /** the name of the topic for customers */
-    private String customersTopicName;
-
-    /**
-     * Default constructor
-     *
-     * @param kafkaAvroProducer the Kafka AVRO producer
-     */
-    @Autowired
-    public DefaultController(CustomerKafkaAvroProducer kafkaAvroProducer, String customersTopicName) {
-        this.kafkaAvroProducer = kafkaAvroProducer;
-        this.customersTopicName = customersTopicName;
-    }
-
     /**
      * Action GET / - Default action - shows the homepage
      *
-     * @return the name of the template to load (homepage.html)
+     * @param model the model to send to the view
+     * @return the name of the template to load (list.html)
      */
-    @GetMapping("/")
-    public String homepageAction() {
-        return "homepage";
-    }
-
-    /**
-     * Action GET /create - Shows the form for creating a cutomer
-     *
-     * @return the name of the template to load (create.html)
-     */
-    @GetMapping("/create")
-    public String createCustomerAction(Model model) {
-        model.addAttribute("customer", new Customer());
-        return "create";
-    }
-
-    /**
-     * Action POST /create - Creates the customer - sends the customer to the Kafka topic
-     *
-     * @param customer the customer data
-     * @return Redirecto to another page
-     */
-    @PostMapping("/create")
-    public ModelAndView createCustomerSubmitAction(@ModelAttribute Customer customer) {
-        try {
-            customer.setUuid(UUID.randomUUID().toString());
-            kafkaAvroProducer.send(customersTopicName, customer);
-        } catch (ExecutionException | InterruptedException exc) {
-            exc.printStackTrace();
-        }
-        return new ModelAndView("redirect:/");
+    @GetMapping({"", "/"})
+    public String homepageAction(Model model) {
+        return "default/homepage";
     }
 }
